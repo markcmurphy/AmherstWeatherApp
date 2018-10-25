57d76286934d3029ae4f026feb0a295d6acfb5ca
Mark Murphy <mark@murphymark.me>
2018-10-25 11:46:38 -0500

    tested API call to get current weather for static ZIP code

--- src/App.js
+++ src/App.js
@@ -2,10 +2,24 @@ import React, { Component } from "react";
 import Input from "./components/input";
 import WeatherCurrent from "./components/current";
 import WeatherForecast from "./components/forecast";
-import logo from "./logo.svg";
 import "./App.css";
 
+const API_KEY = `${process.env.REACT_APP_WEATHER_API_KEY}`;
+
 class App extends Component {
+  componentDidMount() {
+    const getCurrentWeather = async () => {
+      const api_call = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=78759,us&appid=${API_KEY}`
+      );
+
+      const response = await api_call.json();
+
+      console.log(response);
+    };
+    getCurrentWeather();
+  }
+
   render() {
     return (
       <div>
