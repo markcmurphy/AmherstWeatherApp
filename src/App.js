4bba3a95721a942d2a1fb0308b25c51a9b139616
Mark Murphy <mark@murphymark.me>
2018-10-25 10:54:08 -0500

    Added user location input component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Input from "./components/input";
 import WeatherCurrent from "./components/current";
 import WeatherForecast from "./components/forecast";
 import logo from "./logo.svg";
@@ -8,6 +9,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Input />
         <WeatherCurrent />
         <WeatherForecast />
       </div>
---
+++ src/components/input.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Input = props => {
+  return (
+    <div>
+      User location input here <input />
+    </div>
+  );
+};
+
+export default Input;
