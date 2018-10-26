728cda279a8fda88ee434aa9cbda5f9c604f11fe
Mark Murphy <mark@murphymark.me>
2018-10-26 10:00:53 -0500

    corrected usage of class vs. className

--- src/components/current.js
+++ src/components/current.js
@@ -2,16 +2,16 @@ import React from 'react';
 
 const WeatherCurrent = (props) => {
   return (
-      <div class="card">
-      <div class="card-header">
+      <div className="card">
+      <div className="card-header">
       {props.location && <p> Current Weather In {props.location}</p>}
       </div>
       <div>
-      <ul class="list-group list-group-flush">
-      {props.currentTemp && <li class="list-group-item"> Current Temp: {props.currentTemp}</li>}
-      {props.todaysHigh && <li class="list-group-item"> Today's High: {props.todaysHigh}</li>}
-      {props.todaysLow && <li class="list-group-item"> Today's Low: {props.todaysLow}</li>}
-      {props.currentWeather && <li class="list-group-item"> Current Weather: {props.currentWeather}</li>}
+      <ul className="list-group list-group-flush">
+      {props.currentTemp && <li className="list-group-item"> Current Temp: {props.currentTemp}</li>}
+      {props.todaysHigh && <li className="list-group-item"> Today's High: {props.todaysHigh}</li>}
+      {props.todaysLow && <li className="list-group-item"> Today's Low: {props.todaysLow}</li>}
+      {props.currentWeather && <li className="list-group-item"> Current Weather: {props.currentWeather}</li>}
       </ul>
       </div>
       </div>
--- src/components/input.js
+++ src/components/input.js
@@ -2,15 +2,15 @@ import React from "react";
 
 const Input = props => {
   return (
-    <div class="container">
-      <div class="row">
-      <div class="col-sm">
+    <div className="container">
+      <div className="row">
+      <div className="col-sm">
       <form onSubmit={props.loadCurrentWeather}>
         <input className={`form-control ${props.toggledState ? 'dark': null}`} type="text" name="zip" placeholder="ZIP Code" />
         <input className={`form-control ${props.toggledState ? 'dark': null}`} type="text" name="country" placeholder="Country Code" />
-        <button class="btn btn-primary">Get Weather</button>
+        <button className="btn btn-primary">Get Weather</button>
       </form>
-      <button class="btn btn-primary" onClick={props.theme}>Toggle Dark</button>
+      <button className="btn btn-primary" onClick={props.theme}>Toggle Dark</button>
       </div>
       </div>
       </div>
